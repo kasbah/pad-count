734bd5d1958135233a3c02e18e7e6dd60b3d64b9
Kaspar Emanuel <kaspar.emanuel@gmail.com>
2016-07-09 18:49:52 +0100

    Print datasheet urls from octopart

--- octopart.py
+++ octopart.py
@@ -3,17 +3,18 @@ import urllib
 
 from apikey import apikey
 
-url = 'http://octopart.com/api/v3/parts/match?'
-url += '&queries=[{"mpn":"SN74S74N"}]'
+url = 'http://octopart.com/api/v3/parts/search?'
+url += '&q=SOIC'
+url += '&limit=100'
 url += '&apikey={}'.format(apikey)
+url += '&include[]=datasheets'
 
 data = urllib.urlopen(url).read()
 response = json.loads(data)
 
-# print request time (in milliseconds)
-print response['msec']
-
 # print mpn's
 for result in response['results']:
-    for item in result['items']:
-        print item.keys()
+    part = result['item']
+    sheets = part['datasheets']
+    if len(sheets) > 0:
+        print sheets[0]['url']
