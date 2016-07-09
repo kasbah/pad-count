fe5a1391c0a4de7996650c1916a9804c25b81d07
Kaspar Emanuel <kaspar.emanuel@gmail.com>
2016-07-10 00:34:01 +0100

    Add a sha1 check

--- main.py
+++ main.py
@@ -13,40 +13,54 @@ def list_files(prefix, pin_count):
         uids.append(os.path.splitext(os.path.basename(path))[0])
     return uids
 
-if os.path.exists('data/pdfs'):
-    uids = list_files('pdfs', 4)
-else:
-    uids = octopart.get_pdfs('pdfs', 4)
-
-for uid in uids:
-    image_folder = 'data/training/4'
-    rejected_folder = 'data/rejected/uids/4'
-    utils.makedir(image_folder)
-    utils.makedir(rejected_folder)
-
-    output_path = '{}/{}.png'.format(image_folder, uid)
-    rejected_path = 'data/rejected/{}'.format(uid)
-
-    if os.path.exists(output_path) or os.path.exists(rejected_path):
-        continue
-
-    evince = sp.Popen(['evince', 'data/pdfs/4/{}.pdf'.format(uid)], stdout=sp.PIPE)
-
-    try:
-        c = raw_input('{}> '.format(uid))
-    except EOFError:
-        c = 'e'
-
-    if c == 's':
-        #take a screenshot with imagemagick
-        sp.check_call(['import', output_path])
-    elif c == 'r':
-        pass
-    elif c == 'e':
-        evince.kill()
-        print("")
-        sys.exit(0)
+def run(pin_count):
+    if os.path.exists('data/pdfs/{}'.format(pin_count)):
+        uids = list_files('pdfs', pin_count)
     else:
-        sp.check_call(['touch', rejected_path])
+        uids = octopart.get_pdfs('pdfs', pin_count)
 
-    evince.kill()
+    for uid in uids:
+        image_folder = 'data/training/{}'.format(pin_count)
+        rejected_uids_folder = 'data/rejected/uids/{}'.format(pin_count)
+        rejected_sha_folder = 'data/rejected/sha/{}'.format(pin_count)
+        utils.makedir(image_folder)
+        utils.makedir(rejected_uids_folder)
+        utils.makedir(rejected_sha_folder)
+
+        output_path = '{}/{}.png'.format(image_folder, uid)
+        rejected_uid_path = '{}/{}'.format(rejected_uids_folder, uid)
+
+        if os.path.exists(output_path) or os.path.exists(rejected_uid_path):
+            continue
+
+        pdf_path = 'data/pdfs/{}/{}.pdf'.format(pin_count, uid)
+
+        sha = sp.check_output(['sha1sum', pdf_path]).split(' ')[0]
+        rejected_sha_path = '{}/{}'.format(rejected_sha_folder, sha)
+
+        if os.path.exists(rejected_sha_path):
+            continue
+
+        evince = sp.Popen(['evince', pdf_path], stdout=sp.PIPE, stderr=sp.STDOUT)
+
+        try:
+            c = raw_input('{}> '.format(uid))
+        except EOFError:
+            c = 'e'
+
+        if c == 's':
+            #take a screenshot with imagemagick's import
+            sp.check_call(['import', output_path])
+        elif c == 'r':
+            pass
+        elif c == 'e':
+            evince.kill()
+            print("")
+            sys.exit(0)
+        elif c == 'n':
+            sp.check_call(['touch', '{}/{}'.format(rejected_sha_folder, sha)])
+        else:
+            sp.check_call(['touch', rejected_uid_path])
+        evince.kill()
+
+run(4)
