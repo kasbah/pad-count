0e86862c8d9724d82a1cbf67a2518ce26947e484
Kaspar Emanuel <kaspar.emanuel@gmail.com>
2016-07-10 01:09:20 +0100

    Refactor in prep for quota

--- main.py
+++ main.py
@@ -13,33 +13,33 @@ def list_files(prefix, pin_count):
         uids.append(os.path.splitext(os.path.basename(path))[0])
     return uids
 
-def run(pin_count):
+def run(pin_count, quota=10):
     if os.path.exists('data/pdfs/{}'.format(pin_count)):
         uids = list_files('pdfs', pin_count)
     else:
-        uids = octopart.get_pdfs('pdfs', pin_count)
+        uids = octopart.get_pdfs(pin_count, quota)
+
+    image_folder = 'data/training/{}'.format(pin_count)
+    rejected_sha_global_folder = 'data/rejected/sha/global'
+    rejected_sha_local_folder = 'data/rejected/sha/{}'.format(pin_count)
+    utils.makedir(image_folder)
+    utils.makedir(rejected_sha_global_folder)
+    utils.makedir(rejected_sha_local_folder)
 
     for uid in uids:
-        image_folder = 'data/training/{}'.format(pin_count)
-        rejected_uids_folder = 'data/rejected/uids/{}'.format(pin_count)
-        rejected_sha_folder = 'data/rejected/sha/'
-        utils.makedir(image_folder)
-        utils.makedir(rejected_uids_folder)
-        utils.makedir(rejected_sha_folder)
 
         output_path = '{}/{}.png'.format(image_folder, uid)
-        rejected_uid_path = '{}/{}'.format(rejected_uids_folder, uid)
 
-        if os.path.exists(output_path) or os.path.exists(rejected_uid_path):
-            print('ignoring rejected uid {}'.format(uid))
+        if os.path.exists(output_path):
+            print('ignoring existing uid {}'.format(uid))
             continue
 
         pdf_path = 'data/pdfs/{}/{}.pdf'.format(pin_count, uid)
-
         sha = sp.check_output(['sha1sum', pdf_path]).split(' ')[0]
-        rejected_sha_path = '{}/{}'.format(rejected_sha_folder, sha)
+        rejected_sha_global_path = '{}/{}'.format(rejected_sha_global_folder, sha)
+        rejected_sha_local_path = '{}/{}'.format(rejected_sha_local_folder, sha)
 
-        if os.path.exists(rejected_sha_path):
+        if os.path.exists(rejected_sha_global_path) or os.path.exists(rejected_sha_local_path):
             print('ignoring rejected sha1 {}'.format(sha))
             continue
 
@@ -58,11 +58,11 @@ def run(pin_count):
             print("")
             sys.exit(0)
         elif c == 'r':
-            print('rejecting uid: {}'.format(uid))
-            sp.check_call(['touch', rejected_uid_path])
+            print('rejecting sha1 for pin_count of {}: {}'.format(pin_count, sha1))
+            sp.check_call(['touch', rejected_sha_local_path])
         else:
             print('rejecting sha1: {}'.format(sha))
-            sp.check_call(['touch', '{}/{}'.format(rejected_sha_folder, sha)])
+            sp.check_call(['touch', rejected_sha_global_path])
         evince.kill()
 
 run(4)
